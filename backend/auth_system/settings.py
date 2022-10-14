98498dbe58d582eca04a270097f92276d687c65e
Muhammad osama <49996223+osamayy515@users.noreply.github.com>
2022-10-14 16:25:00 +0500

    Update settings.py

--- backend/auth_system/settings.py
+++ backend/auth_system/settings.py
@@ -91,7 +91,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "js03212497@gmail.com"
-EMAIL_HOST_PASSWORD = "apaekhjjwoyvhoup"
 EMAIL_USE_TLS = True
 
 # Password validation
