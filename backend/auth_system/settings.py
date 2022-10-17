6457f425363dfe843524cb60a25f43368063d9c4
Osama saleem <osamasaleem515@gmail.com>
2022-10-17 15:11:31 +0500

    Applying permissions

--- backend/auth_system/settings.py
+++ backend/auth_system/settings.py
@@ -137,6 +137,9 @@ STATIC_ROOT = os.path.join(BASE_DIR,"static")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated'
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
