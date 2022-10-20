53a60a163f61464d59945080116d53727cfc2903
Osama saleem <osamasaleem515@gmail.com>
2022-10-20 17:03:00 +0500

    Building the app & bringing in the cores origin

---
+++ backend/build/precache-manifest.b72e7c3f3451fb5c90672b8936140ee9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "50b0eaac4b0c11f8bfdf",
+    "url": "/static/js/main.50b0eaac.chunk.js"
+  },
+  {
+    "revision": "8740c6cc3f7541bf8b2a",
+    "url": "/static/js/1.8740c6cc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "273f8ae2c4f7fd4717901fc4dcc70eab",
+    "url": "/index.html"
+  }
+];
