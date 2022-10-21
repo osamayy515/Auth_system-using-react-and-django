34f97db08059205fc5cfd064a37d4384095a9fdd
Osama saleem <osamasaleem515@gmail.com>
2022-10-21 17:38:49 +0500

    Edited home container

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 const Home = (props) => (
     <div className='container'>
-        <div className='mt-5 p-5 bg-light text-black rounded'>
+        <div className='mt-5 p-5 bg-info text-black rounded'>
             <h1 className='display-4'>Welcome to Auth System</h1>
             <p className='lead'>This is an incredible authentication system with production level features!</p>
             <hr className='my-4' />
             <p>Click the Log In button</p>
-            <Link className='btn btn-primary btn-lg' to='/login' role='button'>Login</Link>
+            <Link className='btn btn-dark btn-lg' to='/login' role='button'>Login</Link>
         </div>
     </div>
 );
