c152c4653b5a74b1cc5dc4a5cfa9d4085f99a047
Osama saleem <osamasaleem515@gmail.com>
2022-10-24 13:29:35 +0500

    Applying authentication to the navbar component

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,30 +1,56 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { logout } from '../actions/auth';
 
-const Navbar = (props) => (
-    <nav className='navbar navbar-expand-lg bg-dark'>
-        <div className='container-fluid'>
-            <Link className='navbar-brand text-white' to='/'>Auth System</Link>
-            <button 
-                className='navbar-toggler' 
-                type='button' 
-                data-bs-toggle='collapse' 
-                data-bs-target='#navbarNavAltMarkup' 
-                aria-controls='navbarNavAltMarkup' 
-                aria-expanded='false' 
-                aria-label='Toggle navigation'
-            >
-                <span className='navbar-toggler-icon'></span>
-            </button>
-            <div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
-              <div className='navbar-nav'>
-                <Link className='nav-link active text-white' aria-current='page' to='/'>Home</Link>
+const Navbar = ({ logout, isAuthenticated }) => {
+    const guestLinks = () => (
+        <Fragment>
+            <li className='nav-item'>
                 <Link className='nav-link text-white' to='/login'>Login</Link>
-                <Link className='nav-link text-white' to='/signup'>Sign Up</Link>
-              </div>
-            </div>
-        </div>
-    </nav>
-);
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link text-white' to='/signup'>Sign Up</Link>
+            </li>
+        </Fragment>
+    );
+    
+    const authLinks = () => (
+        <li className='nav-item'>
+                <a className='nav-link text-white' href='#!' onClick={logout}>Logout</a>
+        </li>
+    );
 
-export default Navbar;
+    return(
+        <nav className='navbar navbar-expand-lg bg-dark'>
+            <div className='container-fluid'>
+                <Link className='navbar-brand text-white' to='/'>Auth System</Link>
+                <button 
+                    className='navbar-toggler' 
+                    type='button' 
+                    data-bs-toggle='collapse' 
+                    data-bs-target='#navbarNavAltMarkup' 
+                    aria-controls='navbarNavAltMarkup' 
+                    aria-expanded='false' 
+                    aria-label='Toggle navigation'
+                >
+                    <span className='navbar-toggler-icon'></span>
+                </button>
+                <div className='collapse navbar-collapse' id='navbarNav'>
+                  <ul className='navbar-nav'>
+                    <li className='nav-item'>
+                      <Link className='nav-link active text-white' aria-current='page' to='/'>Home</Link>
+                    </li>
+                    {isAuthenticated ? authLinks() : guestLinks()}
+                  </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { logout })(Navbar);
