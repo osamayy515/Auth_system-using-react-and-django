3ad8b30bd0a5d1017c7f3779488267cc8029305b
Osama saleem <osamasaleem515@gmail.com>
2022-10-26 18:10:33 +0500

    Adding reset pass confirm functionality

--- frontend/src/containers/ResetPasswordConfirm.js
+++ frontend/src/containers/ResetPasswordConfirm.js
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { useState} from 'react'
+import { Navigate, useParams } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { reset_password_confirm } from '../actions/auth';
 
-const ResetPasswordConfirm = (props) => (
-    <div>
-        ResetPasswordConfirm
-    </div>
-);
+const ResetPasswordConfirm = ({ match, reset_password_confirm }) => {
 
-export default ResetPasswordConfirm;
+    const [requestSent, setRequestSent] = useState(false);
+    
+    const [formData, setFormData] = useState({
+        new_password: '',
+        re_new_password: ''
+    });
+    
+    const { new_password, re_new_password } = formData;
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = e => {
+        e.preventDefault();
+
+        // const [uid, token] = useParams();
+        const uid = match.params.uid
+        const token = match.params.token
+
+        reset_password_confirm(uid, token, new_password, re_new_password);
+        setRequestSent(true);
+    };
+
+    if (requestSent) {
+        return <Navigate to='/' />
+    }
+    return(
+        <div className='container mt-5'>
+            <form onSubmit={e => onSubmit(e)}>
+                <div className='form-group mb-3'>
+                    <input 
+                        className='form-control' 
+                        type='password' 
+                        placeholder='New Password' 
+                        name='new_password' 
+                        value={new_password} 
+                        onChange={e => onChange(e)}
+                        minLength ='6'
+                        required
+                    />
+                </div>
+                <div className='form-group mb-3'>
+                    <input 
+                        className='form-control' 
+                        type='password' 
+                        placeholder='Confirm New Password' 
+                        name='re_new_password' 
+                        value={re_new_password} 
+                        onChange={e => onChange(e)}
+                        minLength ='6'
+                        required
+                    />
+                </div>
+                <button className='btn btn-dark' type='submit'>Reset Password</button>
+            </form>
+        </div>
+    );
+};
+
+export default connect(null , { reset_password_confirm })(ResetPasswordConfirm);
