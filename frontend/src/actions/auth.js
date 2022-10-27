45bfa73d496c24af8be9fa8eb7256fe775320384
Osama saleem <osamasaleem515@gmail.com>
2022-10-27 15:28:38 +0500

    Added signup and verify functionality

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -6,10 +6,14 @@ import {
     USER_LOADED_FAIL,
     AUTHENTICATED_SUCCESS,
     AUTHENTICATED_FAIL,
+    ACTIVATION_SUCCESS,
+    ACTIVATION_FAIL,
     PASSWORD_RESET_SUCCESS,
     PASSWORD_RESET_FAIL,
     PASSWORD_RESET_CONFIRM_SUCCESS,
     PASSWORD_RESET_CONFIRM_FAIL,
+    SIGNUP_SUCCESS,
+    SIGNUP_FAIL,
     LOGOUT
 } from './types';
 
@@ -102,6 +106,53 @@ export const login = (email, password) => async dispatch => {
     }
 };
 
+export const signup = (name, email, password, re_password) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    
+    const body = JSON.stringify({ name, email, password, re_password });
+
+    try {
+        const res = await axios.post(`${process.env.REACT_APP_API_URL}/auth/users/`, body, config);
+        
+        dispatch({
+            type: SIGNUP_SUCCESS,
+            payload : res.data
+        });
+
+    } catch (err) {
+        dispatch({
+            type: SIGNUP_FAIL,
+        });
+    }
+};
+
+export const verify = (uid, token) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    
+    const body = JSON.stringify({ uid, token });
+
+    try {
+        await axios.post(`${process.env.REACT_APP_API_URL}/auth/users/activation/`, body, config);
+        
+        dispatch({
+            type: ACTIVATION_SUCCESS,
+        });
+
+    } catch (err) {
+        dispatch({
+            type: ACTIVATION_FAIL,
+        });
+    }
+};
+
 export const reset_password = (email) => async dispatch => {
     const config = {
         headers: {
@@ -131,9 +182,9 @@ export const reset_password_confirm = (uid, token, new_password, re_new_password
             'Content-Type': 'application/json'
         }
     };
-
+    
     const body = JSON.stringify({ uid, token, new_password, re_new_password });
-
+    console.log(body);
     try {
         await axios.post(`${process.env.REACT_APP_API_URL}/auth/users/reset_password_confirm/`, body, config);
 
